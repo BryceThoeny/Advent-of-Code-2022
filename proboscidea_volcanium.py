d8089d172e16762336526c68f1e3a4494b4ea6e9
BryceThoeny <117137489+BryceThoeny@users.noreply.github.com>
2022-12-17 14:30:41 -0600

    Update proboscidea_volcanium.py

--- proboscidea_volcanium.py
+++ proboscidea_volcanium.py
@@ -55,29 +55,32 @@ class SimulatedAnnealing:
             self.decrementRule()
 
 class Solution:
-"""
+    """
 Need to redo this to use the new action_cost thing for room/cost lookup, rather than using room objects.
 
 
 Might be OK? Need to review some time
 """
 
-    def __init__(self, move_list, remaining, openers, valve_values, action_cost, pressure_per = 0, total_pressure = 0):
+    def __init__(self, move_list, remaining, openers, valve_values, action_cost, pressure_per = None, total_pressure = None):
 
         #contains a list of the rooms moved to, for the current permutation under consideration
-        move_list = self.move_list
+        self.move_list = move_list
         #contains the remaining time
-        remaining = self.remaining
+        self.remaining = remaining
         #contains a list of the openers
-        openers = self.openers
+        self.openers = openers
         #contains a dictionary which correlates valves as strings to their values as integers
-        valve_values = self.valve_values
+        self.valve_values = valve_values
         #Contains a dictionary of dictionaries used to lookup the cost of actions as action_cost[source][destination]
+        self.action_cost = action_cost
 
         #conatins the pressure relieved per time step
-        pressure_per = self.pressure_per
+        if pressure_per is None:
+            self.pressure_per = 0
         #contains the total pressure relieved
-        total_pressure = self.pressure
+        if total_pressure is None:
+            self.total_pressure = 0
 
 
     def solve(self):
@@ -86,15 +89,15 @@ Might be OK? Need to review some time
         for opener in self.openers:
             opener.load_move(self)
 
-        while not move_list == [] and self.remaining > 0:
-            soonest_opener, latest_opener = soonest()
+        while not self.move_list == [] and self.remaining > 0:
+            soonest_opener, latest_opener = self.soonest()
             soonest_opener.execute_move(self, latest_opener)
             soonest_opener.load_move(self)
 
         if self.remaining > 0:
             self.total_pressure += (self.remaining * self.pressure_per)
 
-        return total_pressure
+        return self.total_pressure
 
     def soonest(self):
 
@@ -115,9 +118,12 @@ class Opener:
     def __init__(self, room, time_in_move = 0, destination = ""):
         
         #the room the opener in question is in
-        room = self.room
-        time_in_move = self.time_in_move
-        destination = self.destination
+        self.room = room
+        if time_in_move is None:
+            self.time_in_move = 0
+        if destination is None:
+            self.destination is ""
+    
 
     def execute_move(self, solution, other_opener):
 
@@ -130,7 +136,7 @@ class Opener:
 
             other_opener.time_in_move -= self.time_in_move
 
-            self.room = destination
+            self.room = self.destination
         else:
             solution.total_pressure += (solution.pressure_per * solution.remaining)
             solution.remaining = 0
@@ -141,10 +147,52 @@ class Opener:
         self.time_in_move = solution.action_costs[self.room][self.destination]
 
 
-def breadth_first_search(source, destination):
-    foo = bar
 
-    #Returns the cost to get to the destination from the source
+class Node:
+
+    def __init__(self, room, map, path = None, cost = None):
+
+        self.room = room
+        self.map = map
+        
+        if path is None:
+            self.path = [self.room]
+        if cost is None:
+            self.cost = 0
+     
+
+    def __repr__(self):
+
+        return f"Node {self.room}"
+
+
+
+def breadth_first_search(source, map, valid_rooms):
+
+    current = Node(source, map, [source])
+    frontier = [current]
+    path_costs = {}
+
+    while True:
+        #Pop the oldest item off of the frontier
+        current = frontier.pop(0)
+        
+        if current.room in valid_rooms:
+            cost = len(current.path)
+            path_costs.update({current.room: cost})
+            
+        else:
+            #If the node popped from the frontier isn't the destination, then add the neighbors to the popped node to the frontier
+            update_frontier(current.room)
+            #And add the popped node to explored
+            explored.add(current.room)
+            
+            
+            
+            
+    
+
+    #Returns a dict containing the destination rooms and costs for all valid rooms given the source room provided
 
 def part1():
     with open("test_input.txt") as file:
@@ -156,22 +204,25 @@ def part1():
         openers = []
         move_list = ["DD", "BB", "JJ", "HH", "EE", "CC"]
         valve_values = {"BB": 13, "CC": 2, "DD": 20, "EE": 3, "HH": 22, "JJ": 21}
+
+        #map is going to be a dict of dicts
+        map = {}
         
         #Still need to figure out how to get input from the file, and determine valid_rooms
      
         #Iterates over all combinations of valid source and destination rooms
         #Calls the BFS function for each one, and updates the path costs with a dict entry showing the cost to get to the keyed destination
         #Final result is a dict of dicts, action_cost, which can be used to lookup the action cost of going from one room to another by action_cost[source][destination]
-        action_cost = {}
+        
         valid_rooms = start_room + valve_values.keys
-        for source_room in valid_rooms:
-            path_costs = {}
-            for destination_room in valid_rooms:
-                path_costs.update({destination_rooom: breadth_first_search(source_room, destination_room)})
 
-            action_cost.update({source_room: path_costs})
+        #iterates over every valid source room, using bfs to generate a dict of the destination rooms and their costs for the given source room
+        action_cost = {}
+        for room in valid_rooms:
+            action_cost.update({room: breadth_first_search(room, map, valid_rooms)})
 
-        solution = Solution(move_list, remaining, openers, valve_values, action_cost)
+
+        solution = Solution(move_list, time, openers, valve_values, action_cost)
 
         print.solution.solve()
 
